3d16e77c2a0a980951f4e37117781cba73663b7c
Maria Iudina <gretarichter@gmail.com>
2025-12-05 11:06:55 +0200

    Add SPA rewrite for Vercel

--- src/index.js
+++ src/index.js
@@ -13,7 +13,12 @@ const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
 app.use(express.json());
 
 app.use(
